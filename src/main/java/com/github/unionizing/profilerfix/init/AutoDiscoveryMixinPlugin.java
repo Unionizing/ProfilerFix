6ff01c5770a5e8952c4212b1a5ca7d46c88280a6
GitHub Action <action@github.com>
2024-05-26 20:47:38 +0000

    Initialize template repository proper
    Set package name to com.github.unionizing.profilerfix
    Set project name to ProfilerFix
    Set modid to profilerfix

--- src/main/java/com/github/unionizing/profilerfix/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/unionizing/profilerfix/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.unionizing.profilerfix.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
